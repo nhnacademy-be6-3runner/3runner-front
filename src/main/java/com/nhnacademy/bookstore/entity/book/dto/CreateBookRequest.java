95728538d866f77372c361bd510ec2bf558050c5
administrator <administrator@administrators-Laptop.local>
2024-06-18 15:08:19 +0900

    fix:CreateBookReqeust

--- src/main/java/com/nhnacademy/bookstore/entity/book/dto/CreateBookRequest.java
+++ src/main/java/com/nhnacademy/bookstore/entity/book/dto/CreateBookRequest.java
@@ -9,16 +9,16 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.ZonedDateTime;
 
 public record CreateBookRequest(
-        @NotBlank(message = "title is entry") String title,
-        @NotBlank String description,
-        @DateTimeFormat ZonedDateTime publishedDate,
-        @Min(0) int price,
-        @Min(0) int quantity,
-        @Min(0) int sellingPrice,
-        @Min(0) int viewCount,
-        @NotNull boolean packing,
-        @NotBlank String author,
-        @NotBlank String isbn,
-        @NotBlank String publisher,
-        @DateTimeFormat ZonedDateTime createdAt) {
+        @NotBlank(message = "title is mandatory") String title,
+        @NotBlank(message = "description is mandatory")  String description,
+        @NotNull(message = "publishedDate is mandatory") ZonedDateTime publishedDate,
+        @Min(value = 0,message = "price must be bigger then 0") int price,
+        @Min(value = 0,message = "quantity must be bigger then 0") int quantity,
+        @Min(value = 0,message = "sellingPrice must be bigger then 0") int sellingPrice,
+        @Min(value = 0,message = "viewCount must be bigger then 0") int viewCount,
+        @NotNull(message = "packing is mandatory") boolean packing,
+        @NotBlank(message = "author is mandatory") String author,
+        @NotBlank(message = "isbn is mandatory") String isbn,
+        @NotBlank(message = "publisher is mandatory") String publisher,
+        @NotNull(message = "createdAt is mandatory") ZonedDateTime createdAt) {
 }
