054442f3e61110f76227e998f97d6d766cabc8de
한민기 <dkssudrhd@naver.com>
2024-06-20 23:12:49 +0900

    fix : merge 오류 수정

--- src/main/java/com/nhnacademy/bookstore/BookStore.java
+++ src/main/java/com/nhnacademy/bookstore/BookStore.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
-public class Application {
+public class BookStore {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(BookStore.class, args);
     }
 
 }
