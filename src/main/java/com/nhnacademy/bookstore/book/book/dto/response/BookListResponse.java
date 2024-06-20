de37ee977b9deff839db97e548993d4e6251dc06
ubiies <alal55al9@naver.com>
2024-06-20 16:11:58 +0900

    feat : dto 수정

---
+++ src/main/java/com/nhnacademy/bookstore/book/book/dto/response/BookListResponse.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.bookstore.book.book.dto.response;
+
+import lombok.Builder;
+
+/**
+ * main page, search page 반환활 도서 리스트 dto
+ * @author 김은비
+ * @param title
+ * @param price
+ * @param sellingPrice
+ * @param author
+ */
+@Builder
+public record BookListResponse(
+        String title, int price, int sellingPrice, String author
+) {
+}
--- src/main/java/com/nhnacademy/bookstore/book/bookCartegory/dto/request/CreateBookCategoryRequest.java
+++ src/main/java/com/nhnacademy/bookstore/book/bookCartegory/dto/request/CreateBookCategoryRequest.java
@@ -2,6 +2,8 @@ package com.nhnacademy.bookstore.book.bookCartegory.dto.request;
 
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
-public record CreateBookCategoryRequest(long bookId, long categoryId) {
+public record CreateBookCategoryRequest(long bookId, List<Long> categoryIds) {
 }
