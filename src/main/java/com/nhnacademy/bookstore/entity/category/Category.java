e6756dd42fb870fe611fe69aaac49a29c143caa0
ubiies <alal55al9@naver.com>
2024-06-25 10:35:42 +0900

    fix : merge 충돌 수정

--- src/main/java/com/nhnacademy/bookstore/entity/category/Category.java
+++ src/main/java/com/nhnacademy/bookstore/entity/category/Category.java
@@ -4,7 +4,10 @@ import com.nhnacademy.bookstore.entity.bookCategory.BookCategory;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,17 +33,16 @@ public class Category {
     // 연결
     @Setter
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "parent", cascade = CascadeType.ALL)
-    @Builder.Default
     private List<Category> children = new ArrayList<>();
 
     @Setter
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
     private List<BookCategory> bookCategoryList = new ArrayList<>();
 
     public void setParent(Category parent) {
         this.parent = parent;
     }
+
     public void addChildren(Category child) {
         if (this.children == null) {
             this.children = new ArrayList<>();
