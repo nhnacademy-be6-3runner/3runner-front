7bd4c90bbb6ad67c3e5cc52b76a1b17f9cef8ee2
okeio <dhdustn7414@gmail.com>
2024-06-27 17:58:49 +0900

    fix: 세터 수정

--- src/main/java/com/nhnacademy/bookstore/member/member/controller/MemberController.java
+++ src/main/java/com/nhnacademy/bookstore/member/member/controller/MemberController.java
@@ -1,5 +1,18 @@
 package com.nhnacademy.bookstore.member.member.controller;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nhnacademy.bookstore.entity.auth.Auth;
 import com.nhnacademy.bookstore.entity.member.Member;
@@ -12,24 +25,13 @@ import com.nhnacademy.bookstore.member.member.dto.request.LoginRequest;
 import com.nhnacademy.bookstore.member.member.dto.request.UpdateMemberRequest;
 import com.nhnacademy.bookstore.member.member.dto.response.GetMemberResponse;
 import com.nhnacademy.bookstore.member.member.dto.response.UpdateMemberResponse;
-import com.nhnacademy.bookstore.member.auth.service.AuthService;
-import com.nhnacademy.bookstore.member.memberAuth.service.MemberAuthService;
-import com.nhnacademy.bookstore.member.memberAuth.service.impl.MemberAuthServiceImpl;
-import com.nhnacademy.bookstore.member.pointRecord.service.PointService;
 import com.nhnacademy.bookstore.member.member.service.impl.MemberServiceImpl;
+import com.nhnacademy.bookstore.member.memberAuth.service.impl.MemberAuthServiceImpl;
 import com.nhnacademy.bookstore.member.pointRecord.service.impl.PointServiceImpl;
 import com.nhnacademy.bookstore.util.ApiResponse;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * The type Member controller.
@@ -39,134 +41,140 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-    private final MemberServiceImpl memberService;
-    private final PointServiceImpl pointRecordService;
-    private final AuthServiceImpl authService;
-    private final MemberAuthServiceImpl memberAuthService;
-    private final PasswordEncoder passwordEncoder;
+	private final MemberServiceImpl memberService;
+	private final PointServiceImpl pointRecordService;
+	private final AuthServiceImpl authService;
+	private final MemberAuthServiceImpl memberAuthService;
+	private final PasswordEncoder passwordEncoder;
 
+	/**
+	 * Create member response entity.- 회원가입에 사용되는 함수이다.
+	 *
+	 * @param request the request - creatememberrequest를 받아 member를 생성한다.
+	 * @author 유지아
+	 */
+	@PostMapping("/bookstore/members")
+	public ApiResponse<Void> createMember(@RequestBody @Valid CreateMemberRequest request) {
+		CreateMemberRequest encodedRequest = new CreateMemberRequest(request.email(),
+			passwordEncoder.encode(request.password()), request.name(), request.phone(), request.age(),
+			request.birthday());
+		Member member = new Member(encodedRequest);
+		Auth auth = authService.getAuth("USER");
+		memberService.save(member);
+		PointRecord pointRecord = new PointRecord(null, 5000L, 5000L, ZonedDateTime.now(), "회원가입 5000포인트 적립.", member);
+		pointRecordService.save(pointRecord);
+		memberAuthService.saveAuth(member, auth);
 
-    /**
-     * Create member response entity.- 회원가입에 사용되는 함수이다.
-     *
-     * @param request the request - creatememberrequest를 받아 member를 생성한다.
-     * @author 유지아
-     */
-    @PostMapping("/bookstore/members")
-    public ApiResponse<Void> createMember(@RequestBody @Valid CreateMemberRequest request) {
-                CreateMemberRequest encodedRequest = new CreateMemberRequest(request.email(),passwordEncoder.encode(request.password()),request.name(),request.phone(),request.age(),request.birthday());
-                Member member = new Member(encodedRequest);
-                Auth auth = authService.getAuth("USER");
-                memberService.save(member);
-                PointRecord pointRecord = new PointRecord(null, 5000L, 5000L, ZonedDateTime.now(), "회원가입 5000포인트 적립.", member);
-                pointRecordService.save(pointRecord);
-                memberAuthService.saveAuth(member, auth);
+		return new ApiResponse<Void>(new ApiResponse.Header(true, 201), new ApiResponse.Body<Void>(null));
+	}
 
-              return new ApiResponse<Void>(new ApiResponse.Header(true,201),new ApiResponse.Body<Void>(null));
-    }
+	/**
+	 * Find by member id response entity. -멤버아이디를 기반으로 멤버정보를 가져온다.
+	 *
+	 * @param memberId the member id -long형인 memberId값을 request header로 받는다.
+	 * @return the response entity -멤버 정보에 대한 응답을 담아서 apiresponse로 응답한다.
+	 * @author 유지아
+	 */
+	@GetMapping("/bookstore/members")
+	public ApiResponse<GetMemberResponse> readById(@RequestHeader("member-id") Long memberId) {
 
-    /**
-     * Find by member id response entity. -멤버아이디를 기반으로 멤버정보를 가져온다.
-     *
-     * @param memberId the member id -long형인 memberId값을 request header로 받는다.
-     * @return the response entity -멤버 정보에 대한 응답을 담아서 apiresponse로 응답한다.
-     * @author 유지아
-     */
-    @GetMapping("/bookstore/members")
-    public ApiResponse<GetMemberResponse> readById(@RequestHeader("member-id") Long memberId) {
+		Member member = memberService.readById(memberId);
+		GetMemberResponse getMemberResponse = GetMemberResponse.builder()
+			.age(member.getAge())
+			.grade(member.getGrade())
+			.point(member.getPoint())
+			.phone(member.getPhone())
+			.created_at(member.getCreatedAt())
+			.birthday(member.getBirthday())
+			.email(member.getEmail())
+			.name(member.getName())
+			.password(member.getPassword()).build();
+		return new ApiResponse<GetMemberResponse>(new ApiResponse.Header(true, 200),
+			new ApiResponse.Body<>(getMemberResponse));
 
-            Member member = memberService.readById(memberId);
-            GetMemberResponse getMemberResponse = GetMemberResponse.builder()
-                    .age(member.getAge())
-                    .grade(member.getGrade())
-                    .point(member.getPoint())
-                    .phone(member.getPhone())
-                    .created_at(member.getCreated_at())
-                    .birthday(member.getBirthday())
-                    .email(member.getEmail())
-                    .name(member.getName())
-                    .password(member.getPassword()).build();
-            return new ApiResponse<GetMemberResponse>(new ApiResponse.Header(true,200),new ApiResponse.Body<>(getMemberResponse));
+	}
 
-    }
+	/**
+	 * Find by email and password response entity. -이메일과 비밀번호에 맞는 멤버정보를 반환한다.
+	 *
+	 * @param
+	 * @return the response entity -멤버 정보에 대한 응답을 담아서 apiresponse로 응답한다.
+	 * @author 유지아
+	 */
+	@PostMapping("/bookstore/members/login")
+	public ApiResponse<GetMemberResponse> readByEmailAndPassword(
+		@RequestBody @Valid LoginRequest loginRequest) {
+		Member member = memberService.readByEmailAndPassword(loginRequest.email(), loginRequest.password());
+		GetMemberResponse getMemberResponse = GetMemberResponse.builder()
+			.age(member.getAge())
+			.grade(member.getGrade())
+			.point(member.getPoint())
+			.phone(member.getPhone())
+			.created_at(member.getCreatedAt())
+			.birthday(member.getBirthday())
+			.email(member.getEmail())
+			.name(member.getName())
+			.password(member.getPassword()).build();
+		memberService.updateStatus(member.getId(), Status.Active);//응답이 만들어 졌다는거는 로그인성공이란 소리니까 멤버를 업데이트 해야할 것같다..
+		memberService.updateLastLogin(member.getId(), ZonedDateTime.now());
 
-    /**
-     * Find by email and password response entity. -이메일과 비밀번호에 맞는 멤버정보를 반환한다.
-     *
-     * @param
-     * @return the response entity -멤버 정보에 대한 응답을 담아서 apiresponse로 응답한다.
-     * @author 유지아
-     */
-    @PostMapping("/bookstore/members/login")
-    public ApiResponse<GetMemberResponse> readByEmailAndPassword(
-            @RequestBody @Valid LoginRequest loginRequest) {
-            Member member = memberService.readByEmailAndPassword(loginRequest.email(), loginRequest.password());
-            GetMemberResponse getMemberResponse = GetMemberResponse.builder()
-                    .age(member.getAge())
-                    .grade(member.getGrade())
-                    .point(member.getPoint())
-                    .phone(member.getPhone())
-                    .created_at(member.getCreated_at())
-                    .birthday(member.getBirthday())
-                    .email(member.getEmail())
-                    .name(member.getName())
-                    .password(member.getPassword()).build();
-            memberService.updateStatus(member.getId(), Status.Active);//응답이 만들어 졌다는거는 로그인성공이란 소리니까 멤버를 업데이트 해야할 것같다..
-            memberService.updateLastLogin(member.getId(),ZonedDateTime.now());
+		return new ApiResponse<GetMemberResponse>(new ApiResponse.Header(true, 200),
+			new ApiResponse.Body<>(getMemberResponse));
 
-            return new ApiResponse<GetMemberResponse>(new ApiResponse.Header(true,200),new ApiResponse.Body<>(getMemberResponse));
+	}
 
-    }
+	/**
+	 * Find auths list. -권한에 대한 리스트를 받아온다.
+	 *
+	 * @param memberId the member id -멤버아이디를 받는다.
+	 * @return the list -해당 유저에 대한 권한들을 응답에 담아 apiresponse로 응답한다.
+	 * @author 유지아
+	 */
+	@GetMapping("/bookstore/members/auths")
+	public ApiResponse<List<AuthResponse>> readAuths(@RequestHeader("member-id") Long memberId) {
 
-    /**
-     * Find auths list. -권한에 대한 리스트를 받아온다.
-     *
-     * @param memberId the member id -멤버아이디를 받는다.
-     * @return the list -해당 유저에 대한 권한들을 응답에 담아 apiresponse로 응답한다.
-     * @author 유지아
-     */
-    @GetMapping("/bookstore/members/auths")
-    public ApiResponse<List<AuthResponse>> readAuths(@RequestHeader("member-id")Long memberId){
+		return new ApiResponse<List<AuthResponse>>(new ApiResponse.Header(true, 200),
+			new ApiResponse.Body<>(memberAuthService.readAllAuths(memberId)
+				.stream()
+				.map(a -> AuthResponse.builder().auth(a.getName()).build())
+				.collect(
+					Collectors.toList())));
 
-            return new ApiResponse<List<AuthResponse>>(new ApiResponse.Header(true,200),
-                    new ApiResponse.Body<>(memberAuthService.readAllAuths(memberId).stream().map(a->AuthResponse.builder().auth(a.getName()).build()).collect(
-                        Collectors.toList())));
+	}
 
-    }
+	/**
+	 * 멤버 업데이트
+	 *
+	 * @param memberId            멤버 id
+	 * @param updateMemberRequest password, name, age, phone, email, birthday
+	 * @return the api response - updateMemberResponse
+	 * @author 오연수
+	 */
+	@PutMapping("/bookstore/members")
+	public ApiResponse<UpdateMemberResponse> updateMember(@RequestHeader(name = "Member-Id") Long memberId,
+		@Valid @RequestBody UpdateMemberRequest updateMemberRequest) {
 
-    /**
-     * 멤버 업데이트
-     *
-     * @param memberId            멤버 id
-     * @param updateMemberRequest password, name, age, phone, email, birthday
-     * @return the api response - updateMemberResponse
-     * @author 오연수
-     */
-    @PutMapping("/bookstore/members")
-    public ApiResponse<UpdateMemberResponse> updateMember(@RequestHeader(name = "Member-Id") Long memberId,
-                                                          @Valid @RequestBody UpdateMemberRequest updateMemberRequest) {
+		Member updatedMember = memberService.updateMember(memberId, updateMemberRequest);
+		UpdateMemberResponse updateMemberResponse = UpdateMemberResponse.builder()
+			.id(String.valueOf(updatedMember.getId()))
+			.name(updatedMember.getName()).build();
+		return new ApiResponse<>(new ApiResponse.Header(true, 200), new ApiResponse.Body<>(updateMemberResponse));
 
-            Member updatedMember = memberService.updateMember(memberId, updateMemberRequest);
-            UpdateMemberResponse updateMemberResponse = UpdateMemberResponse.builder()
-                    .id(String.valueOf(updatedMember.getId()))
-                    .name(updatedMember.getName()).build();
-            return new ApiResponse<>(new ApiResponse.Header(true,200),new ApiResponse.Body<>(updateMemberResponse));
+	}
 
-    }
+	/**
+	 * 멤버 탈퇴 처리
+	 *
+	 * @param memberId 멤버 id
+	 * @return the api response - Void
+	 * @author 오연수
+	 */
+	@DeleteMapping("/bookstore/members")
+	public ApiResponse<Void> deleteMember(@RequestHeader(name = "Member-Id") Long memberId) {
+		memberService.deleteMember(memberId);
 
-    /**
-     * 멤버 탈퇴 처리
-     *
-     * @param memberId 멤버 id
-     * @return the api response - Void
-     * @author 오연수
-     */
-    @DeleteMapping("/bookstore/members")
-    public ApiResponse<Void> deleteMember(@RequestHeader(name = "Member-Id") Long memberId) {
-            memberService.deleteMember(memberId);
+		return new ApiResponse<>(new ApiResponse.Header(true, HttpStatus.NO_CONTENT.value()));
 
-            return new ApiResponse<>(new ApiResponse.Header(true, HttpStatus.NO_CONTENT.value()));
-
-    }
+	}
 }
 
--- src/main/java/com/nhnacademy/bookstore/member/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstore/member/member/service/impl/MemberServiceImpl.java
@@ -1,5 +1,14 @@
 package com.nhnacademy.bookstore.member.member.service.impl;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nhnacademy.bookstore.entity.member.Member;
 import com.nhnacademy.bookstore.entity.member.enums.Grade;
@@ -14,17 +23,6 @@ import com.nhnacademy.bookstore.purchase.purchase.dto.response.ReadPurchaseRespo
 import com.nhnacademy.bookstore.purchase.purchase.repository.PurchaseRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * The type Member service.
@@ -35,156 +33,157 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository;
-    private final PurchaseRepository purchaseRepository;
-    private final PasswordEncoder passwordEncoder;
+	private final MemberRepository memberRepository;
+	private final PurchaseRepository purchaseRepository;
+	private final PasswordEncoder passwordEncoder;
 
-    /**
-     * Save member.
-     *
-     * @param member the member -Member값을 받아온다.
-     * @return the member -저장 후 member값을 그대로 반환한다.
-     * @author 유지아 Save member. -멤버값을 받아와 저장한다.(이메일 중복하는걸로 확인하면 좋을듯)
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public Member save(Member member) {
-        Optional<Member> findmember = memberRepository.findByEmail(member.getEmail());
-        if(findmember.isPresent()){
-            throw new AlreadyExistsEmailException();
-        }
-        return memberRepository.save(member);
-    }
+	/**
+	 * Save member.
+	 *
+	 * @param member the member -Member값을 받아온다.
+	 * @return the member -저장 후 member값을 그대로 반환한다.
+	 * @author 유지아 Save member. -멤버값을 받아와 저장한다.(이메일 중복하는걸로 확인하면 좋을듯)
+	 */
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public Member save(Member member) {
+		Optional<Member> findmember = memberRepository.findByEmail(member.getEmail());
+		if (findmember.isPresent()) {
+			throw new AlreadyExistsEmailException();
+		}
+		return memberRepository.save(member);
+	}
 
-    /**
-     * Find by id member.
-     *
-     * @param id the id -long형인 memberid를 받는다.
-     * @return the member -member 반환
-     * @author 유지아 Find by id member. -memberid를 받아 멤버자체를 가져온다.
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public Member readById(Long id) {
-        Optional<Member> member = memberRepository.findById(id);
-        if(member.isPresent()){
-            return member.get();
-        }else{
-            throw new MemberNotExistsException();
-        }
-    }
+	/**
+	 * Find by id member.
+	 *
+	 * @param id the id -long형인 memberid를 받는다.
+	 * @return the member -member 반환
+	 * @author 유지아 Find by id member. -memberid를 받아 멤버자체를 가져온다.
+	 */
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public Member readById(Long id) {
+		Optional<Member> member = memberRepository.findById(id);
+		if (member.isPresent()) {
+			return member.get();
+		} else {
+			throw new MemberNotExistsException();
+		}
+	}
 
-    /**
-     * Find by email and password member.
-     *
-     * @param email    the email -string 이메일 값을 받는다.
-     * @param password the password -string 비밀번호 값을 받는다.
-     * @return the member -해당하는 member를 반환한다.
-     * @author 유지아 Find by email and password member. -이메일과 패스워드 값으로 조회한다.
-     */
-    public Member readByEmailAndPassword(String email, String password) {
-        Optional<Member> member = memberRepository.findByEmail(email);
-        if(member.isPresent()){
-            if(passwordEncoder.matches(password, member.get().getPassword())){
-                return member.get();
-            }
-        }
-        throw new LoginFailException();
-    }
+	/**
+	 * Find by email and password member.
+	 *
+	 * @param email    the email -string 이메일 값을 받는다.
+	 * @param password the password -string 비밀번호 값을 받는다.
+	 * @return the member -해당하는 member를 반환한다.
+	 * @author 유지아 Find by email and password member. -이메일과 패스워드 값으로 조회한다.
+	 */
+	public Member readByEmailAndPassword(String email, String password) {
+		Optional<Member> member = memberRepository.findByEmail(email);
+		if (member.isPresent()) {
+			if (passwordEncoder.matches(password, member.get().getPassword())) {
+				return member.get();
+			}
+		}
+		throw new LoginFailException();
+	}
 
-    /**
-     * 멤버 업데이트
-     *
-     * @param memberId            the member id
-     * @param updateMemberRequest password, name, age, phone, email, birthday
-     * @return the member
-     * @author 오연수
-     */
-    public Member updateMember(Long memberId, UpdateMemberRequest updateMemberRequest) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
+	/**
+	 * 멤버 업데이트
+	 *
+	 * @param memberId            the member id
+	 * @param updateMemberRequest password, name, age, phone, email, birthday
+	 * @return the member
+	 * @author 오연수
+	 */
+	public Member updateMember(Long memberId, UpdateMemberRequest updateMemberRequest) {
+		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
 
-        member.setPassword(updateMemberRequest.password());
-        member.setName(updateMemberRequest.name());
-        member.setAge(updateMemberRequest.age());
-        member.setEmail(updateMemberRequest.email());
-        member.setPhone(updateMemberRequest.phone());
-        member.setBirthday(updateMemberRequest.birthday());
-        member.setModified_at(ZonedDateTime.now());
+		member.setPassword(updateMemberRequest.password());
+		member.setName(updateMemberRequest.name());
+		member.setAge(updateMemberRequest.age());
+		member.setEmail(updateMemberRequest.email());
+		member.setPhone(updateMemberRequest.phone());
+		member.setBirthday(updateMemberRequest.birthday());
+		member.setModifiedAt(ZonedDateTime.now());
 
-        return memberRepository.save(member);
-    }
+		return memberRepository.save(member);
+	}
 
-    /**
-     * 멤버 탈퇴
-     *
-     * @param memberId the member id
-     * @author 오연수
-     */
-    public void deleteMember(Long memberId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
+	/**
+	 * 멤버 탈퇴
+	 *
+	 * @param memberId the member id
+	 * @author 오연수
+	 */
+	public void deleteMember(Long memberId) {
+		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
 
-        member.setStatus(Status.Withdrawn);
-        member.setDeleted_at(ZonedDateTime.now());
+		member.setStatus(Status.Withdrawn);
+		member.setDeletedAt(ZonedDateTime.now());
 
-        memberRepository.save(member);
-    }
+		memberRepository.save(member);
+	}
 
-    /**
-     * Update member's status(활성, 탈퇴, 휴면).
-     *
-     * @param memberId the member id
-     * @param status   the status
-     * @return the member
-     * @author 오연수
-     */
-    public Member updateStatus(Long memberId, Status status) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
-        member.setStatus(status);
-        member.setModified_at(ZonedDateTime.now());
-        return memberRepository.save(member);
-    }
+	/**
+	 * Update member's status(활성, 탈퇴, 휴면).
+	 *
+	 * @param memberId the member id
+	 * @param status   the status
+	 * @return the member
+	 * @author 오연수
+	 */
+	public Member updateStatus(Long memberId, Status status) {
+		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
+		member.setStatus(status);
+		member.setModifiedAt(ZonedDateTime.now());
+		return memberRepository.save(member);
+	}
 
-    /**
-     * Update member's grade(general, royal, gold, platinum).
-     *
-     * @param memberId the member id
-     * @param grade    the grade
-     * @return the member
-     * @author 오연수
-     */
-    public Member updateGrade(Long memberId, Grade grade) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
-        member.setGrade(grade);
-        member.setModified_at(ZonedDateTime.now());
-        return memberRepository.save(member);
-    }
+	/**
+	 * Update member's grade(general, royal, gold, platinum).
+	 *
+	 * @param memberId the member id
+	 * @param grade    the grade
+	 * @return the member
+	 * @author 오연수
+	 */
+	public Member updateGrade(Long memberId, Grade grade) {
+		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
+		member.setGrade(grade);
+		member.setModifiedAt(ZonedDateTime.now());
+		return memberRepository.save(member);
+	}
 
-    @Override
-    public Member updateLastLogin(Long memberId, ZonedDateTime lastLogin) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
-        member.setLast_login_date(lastLogin);
-        return memberRepository.save(member);
-    }
-    /**
-     * 주문 리스트 조회 멤버.
-     *
-     * @param memberId 맴버아이디
-     * @return 리스트
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public List<ReadPurchaseResponse> getPurchasesByMemberId(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(MemberNotExistsException::new);
-        return purchaseRepository.findByMember(member)
-                .stream()
-                .map(purchase -> ReadPurchaseResponse.builder()
-                                    .id(purchase.getId())
-                                    .status(purchase.getStatus())
-                                    .deliveryPrice(purchase.getDeliveryPrice())
-                                    .totalPrice(purchase.getTotalPrice())
-                                    .createdAt(purchase.getCreatedAt())
-                                    .road(purchase.getRoad())
-                                    .password(purchase.getPassword())
-                                    .memberType(purchase.getMemberType())
-                                    .build()
-                ).collect(Collectors.toList());
-    }
+	@Override
+	public Member updateLastLogin(Long memberId, ZonedDateTime lastLogin) {
+		Member member = memberRepository.findById(memberId).orElseThrow(MemberNotExistsException::new);
+		member.setLastLoginDate(lastLogin);
+		return memberRepository.save(member);
+	}
+
+	/**
+	 * 주문 리스트 조회 멤버.
+	 *
+	 * @param memberId 맴버아이디
+	 * @return 리스트
+	 */
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public List<ReadPurchaseResponse> getPurchasesByMemberId(Long memberId) {
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(MemberNotExistsException::new);
+		return purchaseRepository.findByMember(member)
+			.stream()
+			.map(purchase -> ReadPurchaseResponse.builder()
+				.id(purchase.getId())
+				.status(purchase.getStatus())
+				.deliveryPrice(purchase.getDeliveryPrice())
+				.totalPrice(purchase.getTotalPrice())
+				.createdAt(purchase.getCreatedAt())
+				.road(purchase.getRoad())
+				.password(purchase.getPassword())
+				.memberType(purchase.getMemberType())
+				.build()
+			).collect(Collectors.toList());
+	}
 }
