49cb41ca547c6929cfad2859fe45fc77310146fa
ubiies <alal55al9@naver.com>
2024-06-27 18:02:29 +0900

    merge 수정

--- src/main/java/com/nhnacademy/bookstore/book/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/nhnacademy/bookstore/book/book/service/impl/BookServiceImpl.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 책 서비스 구현체.
@@ -22,34 +24,34 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
-	/**
-	 * 책 등록 기능.
-	 *
-	 * @param createBookRequest createBookRequest form
-	 */
-	// Dto -> save book
-	@Override
-	@Transactional(propagation = Propagation.MANDATORY)
-	public Long createBook(CreateBookRequest createBookRequest) {
-		Book book = new Book(
-			createBookRequest.title(),
-			createBookRequest.description(),
-			createBookRequest.publishedDate(),
-			createBookRequest.price(),
-			createBookRequest.quantity(),
-			createBookRequest.sellingPrice(),
-			0,
-			createBookRequest.packing(),
-			createBookRequest.author(),
-			createBookRequest.isbn(),
-			createBookRequest.publisher(),
-			null,
-			null,
-			null
-		);
-		bookRepository.save(book);
-		return book.getId();
-	}
+    /**
+     * 책 등록 기능.
+     *
+     * @param createBookRequest createBookRequest form
+     */
+    // Dto -> save book
+    @Override
+    @Transactional(propagation = Propagation.MANDATORY)
+    public Long createBook(CreateBookRequest createBookRequest) {
+        Book book = new Book(
+                createBookRequest.title(),
+                createBookRequest.description(),
+                createBookRequest.publishedDate(),
+                createBookRequest.price(),
+                createBookRequest.quantity(),
+                createBookRequest.sellingPrice(),
+                0,
+                createBookRequest.packing(),
+                createBookRequest.author(),
+                createBookRequest.isbn(),
+                createBookRequest.publisher(),
+                null,
+                null,
+                null
+        );
+        bookRepository.save(book);
+        return book.getId();
+    }
 
     /**
      * 책 조회 기능.
