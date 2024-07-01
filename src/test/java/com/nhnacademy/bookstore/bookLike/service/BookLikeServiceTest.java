fe5b560dfcf6aaffc814161e0c18e73468c64dc8
ubiies <alal55al9@naver.com>
2024-07-01 16:36:26 +0900

    refactor : SonarLint 수정

--- src/test/java/com/nhnacademy/bookstore/bookLike/service/BookLikeServiceTest.java
+++ src/test/java/com/nhnacademy/bookstore/bookLike/service/BookLikeServiceTest.java
@@ -28,8 +28,8 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -54,31 +54,19 @@ public class BookLikeServiceTest {
     private BookLikeServiceImpl bookLikeService;
 
     private Member member1;
-    private Member member2;
     private Book book1;
-    private Book book2;
     private BookLike bookLike1;
-    private BookLike bookLike2;
-    private BookLike bookLike3;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         member1 = new Member(CreateMemberRequest.builder().password("1").name("1").age(1).phone("1").birthday(ZonedDateTime.now()).email("dfdaf@nav.com").build());
-
-        member2 = new Member(CreateMemberRequest.builder().password("1").name("1").age(1).phone("1").birthday(ZonedDateTime.now()).email("dfdaf2@nav.com").build());
-
         book1 = new Book("책1", "책1입니다.", ZonedDateTime.now(), 1000, 10, 900, 0, true, "작가", "123456789", "출판사", null, null, null);
-
-        book2 = new Book("책2", "책2입니다.", ZonedDateTime.now(), 1000, 10, 900, 0, true, "작가r", "123456789", "출판사", null, null, null);
-
         bookLike1 = new BookLike(1L, null, book1, member1);
-        bookLike2 = new BookLike(2L, null, book2, member2);
-        bookLike3 = new BookLike(3L, null, book1, member2);
     }
 
     @DisplayName("좋아요 아이디 찾기")
     @Test
-    public void testFindById_existById() {
+    void testFindById_existById() {
         given(bookLikeRepository.findById(1L)).willReturn(Optional.of(bookLike1));
 
         BookLike foundBookLike = bookLikeService.findById(1L);
@@ -90,7 +78,7 @@ public class BookLikeServiceTest {
 
     @DisplayName("존재하지 않는 좋아요 아이디 찾기")
     @Test
-    public void testFindById_notExistById() {
+    void testFindById_notExistById() {
         given(bookLikeRepository.findById(1L)).willReturn(Optional.empty());
 
         assertThrows(BookLikeNotExistsException.class, () -> bookLikeService.findById(1L));
@@ -98,18 +86,18 @@ public class BookLikeServiceTest {
 
     @DisplayName("좋아요 생성 성공")
     @Test
-    public void testCreateBookLike() {
+    void testCreateBookLike() {
         given(memberRepository.findById(1L)).willReturn(Optional.of(member1));
         given(bookRepository.findById(1L)).willReturn(Optional.of(book1));
 
         long bookLikeId = bookLikeService.createBookLike(1L, 1L);
 
-        assertTrue(bookLikeId == 0L);
+        assertEquals(0L, bookLikeId);
     }
 
     @DisplayName("존재하지 않는 멤버로 좋아요 추가")
     @Test
-    public void testCreateBookLike_notExistByMember() {
+    void testCreateBookLike_notExistByMember() {
         given(memberRepository.findById(1L)).willReturn(Optional.empty());
 
         assertThrows(MemberNotExistsException.class, () -> bookLikeService.createBookLike(1L, 1L));
@@ -117,7 +105,7 @@ public class BookLikeServiceTest {
 
     @DisplayName("좋아요 삭제 성공")
     @Test
-    public void testDeleteBookLike_existById() {
+    void testDeleteBookLike_existById() {
         given(bookLikeRepository.existsById(1L)).willReturn(true);
 
         bookLikeService.deleteBookLike(1L);
@@ -127,14 +115,14 @@ public class BookLikeServiceTest {
 
     @DisplayName("존재하지 않는 좋아요 삭제")
     @Test
-    public void testDeleteBookLike_notExistById() {
+    void testDeleteBookLike_notExistById() {
         given(bookLikeRepository.existsById(1L)).willReturn(false);
 
         assertThrows(BookLikeNotExistsException.class, () -> bookLikeService.deleteBookLike(1L));
     }
 
     @Test
-    public void testFindBookLikeByMemberId() {
+    void testFindBookLikeByMemberId() {
         Pageable pageable = PageRequest.of(0, 10);
         BookListResponse bookListResponse1 = new BookListResponse(1, "책1", 1000, 900, "작가", "url1");
         BookListResponse bookListResponse2 = new BookListResponse(2, "책2", 1000, 900, "작가r", "url2");
@@ -148,7 +136,7 @@ public class BookLikeServiceTest {
     }
 
     @Test
-    public void testCountLikeByBookId() {
+    void testCountLikeByBookId() {
         given(bookLikeRepository.countLikeByBookId(1L)).willReturn(10L);
 
         long count = bookLikeService.countLikeByBookId(1L);
@@ -158,7 +146,7 @@ public class BookLikeServiceTest {
     }
 
     @Test
-    public void testFindBooksOrderByLikes() {
+    void testFindBooksOrderByLikes() {
         Pageable pageable = PageRequest.of(0, 10);
         BookListResponse bookListResponse1 = new BookListResponse(1, "책1", 1000, 900, "작가", "url1");
         BookListResponse bookListResponse2 = new BookListResponse(2, "책2", 1000, 900, "작가r", "url2");
