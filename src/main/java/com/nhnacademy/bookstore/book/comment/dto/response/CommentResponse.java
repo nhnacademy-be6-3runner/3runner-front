fcc86c5455acc15dd216039ee661e54e80257d44
ubiies <alal55al9@naver.com>
2024-07-02 17:11:17 +0900

    feat : comment custom repository 추가

--- src/main/java/com/nhnacademy/bookstore/book/comment/dto/response/CommentResponse.java
+++ src/main/java/com/nhnacademy/bookstore/book/comment/dto/response/CommentResponse.java
@@ -2,6 +2,13 @@ package com.nhnacademy.bookstore.book.comment.dto.response;
 
 import lombok.Builder;
 
+import java.time.ZonedDateTime;
+
 @Builder
-public record CommentResponse() {
+public record CommentResponse(
+        long commentId,
+        String content,
+        String memberEmail,
+        ZonedDateTime createdAt
+) {
 }
