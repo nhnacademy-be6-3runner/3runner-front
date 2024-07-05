1b2cd153a407069166ffefa5017a5b8406bfee28
한민기 <dkssudrhd@naver.com>
2024-07-05 13:10:03 +0900

    fix : 오류 수정

--- src/main/java/com/nhnacademy/front/book/categroy/controller/CategoryManagementController.java
+++ src/main/java/com/nhnacademy/front/book/categroy/controller/CategoryManagementController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nhnacademy.front.book.categroy.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping
 @Controller
 @RequiredArgsConstructor
 public class CategoryManagementController {
@@ -18,7 +20,7 @@ public class CategoryManagementController {
 
 	@GetMapping("/admin/category/management")
 	public String categoryManagement(Model model) {
-		return "/admin/admin_category";
+		return "admin/admin_category";
 	}
 
 	@GetMapping("/admin/category/delete/{categoryId}")
