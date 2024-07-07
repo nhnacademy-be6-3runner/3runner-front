7973affedbf8f3bf2f8e5d81c1b29e8a205c45da
ubiies <alal55al9@naver.com>
2024-07-07 21:33:35 +0900

    feat : 도서 정렬 추가

--- src/main/java/com/nhnacademy/bookstore/book/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/bookstore/book/book/controller/BookController.java
@@ -1,27 +1,7 @@
 package com.nhnacademy.bookstore.book.book.controller;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.nhnacademy.bookstore.book.book.dto.request.CreateBookRequest;
-import com.nhnacademy.bookstore.book.book.dto.response.BookForCouponResponse;
-import com.nhnacademy.bookstore.book.book.dto.response.BookListResponse;
-import com.nhnacademy.bookstore.book.book.dto.response.BookManagementResponse;
-import com.nhnacademy.bookstore.book.book.dto.response.ReadBookResponse;
-import com.nhnacademy.bookstore.book.book.dto.response.UserReadBookResponse;
+import com.nhnacademy.bookstore.book.book.dto.response.*;
 import com.nhnacademy.bookstore.book.book.exception.CreateBookRequestFormException;
 import com.nhnacademy.bookstore.book.book.exception.UpdateBookRequestFormException;
 import com.nhnacademy.bookstore.book.book.service.BookService;
@@ -32,146 +12,178 @@ import com.nhnacademy.bookstore.book.bookTag.service.BookTagService;
 import com.nhnacademy.bookstore.book.category.dto.response.CategoryParentWithChildrenResponse;
 import com.nhnacademy.bookstore.util.ApiResponse;
 import com.nhnacademy.bookstore.util.ValidationUtils;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 책 요청 컨트롤러.
  *
  * @author 김병우
- * fix 한민기
+ * fix 한민기, 김은비
  */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bookstore/books")
 @Slf4j
 public class BookController {
-	private final BookService bookService;
-	private final BookTagService bookTagService;
-	private final BookCategoryService bookCategoryService;
+    private final BookService bookService;
+    private final BookTagService bookTagService;
+    private final BookCategoryService bookCategoryService;
 
-	/**
-	 * 책 등록 요청 처리.
-	 *
-	 * @param createBookRequest request form
-	 * @param bindingResult     binding result
-	 * @return ApiResponse<>
-	 */
-	@PostMapping
-	public ApiResponse<Void> createBook(@Valid @RequestBody CreateBookRequest createBookRequest,
-		BindingResult bindingResult) {
-		ValidationUtils.validateBindingResult(bindingResult,
-			new CreateBookRequestFormException("책의 항목들의 범위가 잘못되었습니다."));
+    /**
+     * 책 등록 요청 처리.
+     *
+     * @param createBookRequest request form
+     * @param bindingResult     binding result
+     * @return ApiResponse<>
+     */
+    @PostMapping
+    public ApiResponse<Void> createBook(@Valid @RequestBody CreateBookRequest createBookRequest,
+                                        BindingResult bindingResult) {
+        ValidationUtils.validateBindingResult(bindingResult,
+                new CreateBookRequestFormException("책의 항목들의 범위가 잘못되었습니다."));
 
-		bookService.createBook(createBookRequest);
+        bookService.createBook(createBookRequest);
 
-		return new ApiResponse<>(new ApiResponse.Header(true, 201));
-	}
+        return new ApiResponse<>(new ApiResponse.Header(true, 201));
+    }
 
-	@GetMapping
-	public ApiResponse<Page<BookListResponse>> readAllBooks(@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "10") int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		Page<BookListResponse> bookList = bookService.readAllBooks(pageable);
-		return new ApiResponse<>(
-			new ApiResponse.Header(true, 200),
-			new ApiResponse.Body<>(bookList)
-		);
-	}
+    /**
+     * 도서 페이지 전체 조회 메서드입니다.
+     *
+     * @param page 페이지
+     * @param size 사이즈
+     * @param sort 정렬 기준
+     * @return 도서 리스트
+     * @author 김은비
+     */
+    @GetMapping
+    public ApiResponse<Page<BookListResponse>> readAllBooks(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(defaultValue = "publishedDate,desc") String sort) {
 
-	/**
-	 * 책 상세보기 -> 조회수가 올라갑니다.
-	 * @param bookId 책 아이디
-	 * @return 책 조회 정보
-	 */
-	@GetMapping("/{bookId}")
-	public ApiResponse<UserReadBookResponse> readBook(@PathVariable("bookId") Long bookId) {
-		ReadBookResponse detailBook = bookService.readBookById(bookId);
-		List<CategoryParentWithChildrenResponse> categoryList = bookCategoryService.readBookWithCategoryList(bookId);
+        log.info("sort : {}", sort);
 
-		List<ReadTagByBookResponse> tagList =
-			bookTagService.readTagByBookId(
-				ReadBookIdRequest.builder().bookId(bookId).build());
+        String[] sortParams = sort.split(",");
+        String property = sortParams[0];
+        String direction = sortParams.length > 1 ? sortParams[1].toUpperCase() : "DESC";
+        Sort.Direction directionEnum = Sort.Direction.valueOf(direction);
 
-		UserReadBookResponse book = UserReadBookResponse.builder()
-			.id(detailBook.id())
-			.title(detailBook.title())
-			.description(detailBook.description())
-			.publishedDate(detailBook.publishedDate())
-			.price(detailBook.price())
-			.quantity(detailBook.quantity())
-			.sellingPrice(detailBook.sellingPrice())
-			.viewCount(detailBook.viewCount())
-			.packing(detailBook.packing())
-			.author(detailBook.author())
-			.isbn(detailBook.isbn())
-			.publisher(detailBook.publisher())
-			.imagePath(detailBook.imagePath())
-			.categoryList(categoryList)
-			.tagList(tagList)
-			.build();
-		return ApiResponse.success(book);
-	}
+        Sort sortOrder = Sort.by(new Sort.Order(directionEnum, property));
+        Pageable pageable = PageRequest.of(page, size, sortOrder);
 
-	/**
-	 * 책 수정 관련 컨트롤러
-	 * @param bookId 수정할 책의 아이디
-	 * @param createBookRequest 수정 받아 올 정보
-	 * @param bindingResult 정보를 검증할 내용
-	 * @return 완료 코드
-	 */
-	@PutMapping("/{bookId}")
-	public ApiResponse<Void> updateBook(@PathVariable Long bookId,
-		@Valid @RequestBody CreateBookRequest createBookRequest, BindingResult bindingResult) {
+        log.info("정렬 기준 : {}", pageable);
 
-		ValidationUtils.validateBindingResult(bindingResult,
-			new UpdateBookRequestFormException("책의 항목들의 범위가 잘못되었습니다."));
+        Page<BookListResponse> bookList = bookService.readAllBooks(pageable);
+        return ApiResponse.success(bookList);
+    }
 
-		bookService.updateBook(bookId, createBookRequest);
+    /**
+     * 책 상세보기 -> 조회수가 올라갑니다.
+     *
+     * @param bookId 책 아이디
+     * @return 책 조회 정보
+     */
+    @GetMapping("/{bookId}")
+    public ApiResponse<UserReadBookResponse> readBook(@PathVariable("bookId") Long bookId) {
+        ReadBookResponse detailBook = bookService.readBookById(bookId);
+        List<CategoryParentWithChildrenResponse> categoryList = bookCategoryService.readBookWithCategoryList(bookId);
 
-		return new ApiResponse<>(new ApiResponse.Header(true, 200));
-	}
+        List<ReadTagByBookResponse> tagList =
+                bookTagService.readTagByBookId(
+                        ReadBookIdRequest.builder().bookId(bookId).build());
 
-	/**
-	 * 책 삭제 관련 컨트롤러
-	 * @param bookId 삭제할 책의 아이디
-	 * @return 삭제
-	 */
-	@DeleteMapping("/{bookId}")
-	public ApiResponse<Void> deleteBook(@PathVariable Long bookId) {
-		bookService.deleteBook(bookId);
+        UserReadBookResponse book = UserReadBookResponse.builder()
+                .id(detailBook.id())
+                .title(detailBook.title())
+                .description(detailBook.description())
+                .publishedDate(detailBook.publishedDate())
+                .price(detailBook.price())
+                .quantity(detailBook.quantity())
+                .sellingPrice(detailBook.sellingPrice())
+                .viewCount(detailBook.viewCount())
+                .packing(detailBook.packing())
+                .author(detailBook.author())
+                .isbn(detailBook.isbn())
+                .publisher(detailBook.publisher())
+                .imagePath(detailBook.imagePath())
+                .categoryList(categoryList)
+                .tagList(tagList)
+                .build();
+        return ApiResponse.success(book);
+    }
 
-		return new ApiResponse<>(new ApiResponse.Header(true, 200));
-	}
+    /**
+     * 책 수정 관련 컨트롤러
+     *
+     * @param bookId            수정할 책의 아이디
+     * @param createBookRequest 수정 받아 올 정보
+     * @param bindingResult     정보를 검증할 내용
+     * @return 완료 코드
+     */
+    @PutMapping("/{bookId}")
+    public ApiResponse<Void> updateBook(@PathVariable Long bookId,
+                                        @Valid @RequestBody CreateBookRequest createBookRequest, BindingResult bindingResult) {
 
-	/**
-	 * 쿠폰에서 필요한 책의 아이디들로 책들을 검색하는 Api
-	 * @param ids    책의 id list
-	 * @return 검색된 책의 list
-	 */
-	@GetMapping("/list")
-	public ApiResponse<List<BookForCouponResponse>> readAllBooksForCoupon(@RequestParam List<Long> ids) {
-		List<BookForCouponResponse> response = bookService.readBookByIds(ids);
-		return ApiResponse.success(response);
-	}
+        ValidationUtils.validateBindingResult(bindingResult,
+                new UpdateBookRequestFormException("책의 항목들의 범위가 잘못되었습니다."));
 
-	/**
-	 * 관리자 페이지에서 확인할 내용
-	 * @param page 페이지
-	 * @param size 페이지 사이즈
-	 * @return 관리자 페이지에서 사용할 책 리스트
-	 */
-	@GetMapping("/admin")
-	public ApiResponse<Page<BookManagementResponse>> readAllAdminBooks(@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "10") int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		Page<BookManagementResponse> bookList = bookService.readAllAdminBooks(pageable);
-		return new ApiResponse<>(
-			new ApiResponse.Header(true, 200),
-			new ApiResponse.Body<>(bookList)
-		);
-	}
+        bookService.updateBook(bookId, createBookRequest);
+
+        return new ApiResponse<>(new ApiResponse.Header(true, 200));
+    }
+
+    /**
+     * 책 삭제 관련 컨트롤러
+     *
+     * @param bookId 삭제할 책의 아이디
+     * @return 삭제
+     */
+    @DeleteMapping("/{bookId}")
+    public ApiResponse<Void> deleteBook(@PathVariable Long bookId) {
+        bookService.deleteBook(bookId);
+
+        return new ApiResponse<>(new ApiResponse.Header(true, 200));
+    }
+
+    /**
+     * 쿠폰에서 필요한 책의 아이디들로 책들을 검색하는 Api
+     *
+     * @param ids 책의 id list
+     * @return 검색된 책의 list
+     */
+    @GetMapping("/list")
+    public ApiResponse<List<BookForCouponResponse>> readAllBooksForCoupon(@RequestParam List<Long> ids) {
+        List<BookForCouponResponse> response = bookService.readBookByIds(ids);
+        return ApiResponse.success(response);
+    }
+
+    /**
+     * 관리자 페이지에서 확인할 내용
+     *
+     * @param page 페이지
+     * @param size 페이지 사이즈
+     * @return 관리자 페이지에서 사용할 책 리스트
+     */
+    @GetMapping("/admin")
+    public ApiResponse<Page<BookManagementResponse>> readAllAdminBooks(@RequestParam(defaultValue = "0") int page,
+                                                                       @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<BookManagementResponse> bookList = bookService.readAllAdminBooks(pageable);
+        return new ApiResponse<>(
+                new ApiResponse.Header(true, 200),
+                new ApiResponse.Body<>(bookList)
+        );
+    }
 
 }
--- src/main/java/com/nhnacademy/bookstore/book/book/repository/impl/BookCustomRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookstore/book/book/repository/impl/BookCustomRepositoryImpl.java
@@ -1,13 +1,5 @@
 package com.nhnacademy.bookstore.book.book.repository.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
-
 import com.nhnacademy.bookstore.book.book.dto.response.BookListResponse;
 import com.nhnacademy.bookstore.book.book.dto.response.BookManagementResponse;
 import com.nhnacademy.bookstore.book.book.dto.response.ReadBookResponse;
@@ -15,103 +7,188 @@ import com.nhnacademy.bookstore.book.book.repository.BookCustomRepository;
 import com.nhnacademy.bookstore.entity.book.QBook;
 import com.nhnacademy.bookstore.entity.bookImage.QBookImage;
 import com.nhnacademy.bookstore.entity.bookImage.enums.BookImageType;
+import com.nhnacademy.bookstore.entity.bookLike.QBookLike;
 import com.nhnacademy.bookstore.entity.totalImage.QTotalImage;
 import com.nhnacademy.bookstore.purchase.purchaseBook.exception.NotExistsBook;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 도서 커스텀 레포지토리입니다.
+ *
+ * @author 김은비
+ */
+@Slf4j
 @Repository
 public class BookCustomRepositoryImpl implements BookCustomRepository {
-	private final JPAQueryFactory jpaQueryFactory;
-	private final QBook qBook = QBook.book;
-	private final QBookImage qBookImage = QBookImage.bookImage;
-	private final QTotalImage qTotalImage = QTotalImage.totalImage;
+    private final JPAQueryFactory jpaQueryFactory;
+    private static final QBook qBook = QBook.book;
+    private static final QBookImage qBookImage = QBookImage.bookImage;
+    private static final QTotalImage qTotalImage = QTotalImage.totalImage;
+    private static final QBookLike qBookLike = QBookLike.bookLike;
 
-	public BookCustomRepositoryImpl(EntityManager entityManager) {
-		this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-	}
+    public BookCustomRepositoryImpl(EntityManager entityManager) {
+        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
+    }
 
-	@Override
-	public Page<BookListResponse> readBookList(Pageable pageable) {
-		List<BookListResponse> content = jpaQueryFactory.select(
-				Projections.constructor(BookListResponse.class,
-					qBook.id,
-					qBook.title,
-					qBook.price,
-					qBook.sellingPrice,
-					qBook.author,
-					qTotalImage.url))
-			.from(qBook)
-			.leftJoin(qBookImage)
-			.on(qBookImage.book.id.eq(qBook.id).and(qBookImage.type.eq(BookImageType.MAIN)))
-			.leftJoin(qTotalImage)
-			.on(qTotalImage.bookImage.id.eq(qBookImage.id))
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
-			.fetch();
-		long total = Optional.ofNullable(
-			jpaQueryFactory.select(qBook.count())
-				.from(qBook)
-				.fetchOne()
-		).orElse(0L);
-		return new PageImpl<>(content, pageable, total);
-	}
+    /**
+     * 도서 리스트를 불러오는 쿼리입니다.
+     *
+     * @param pageable 페이지
+     * @return 도서 리스트
+     * @author 김은비
+     */
+    @Override
+    public Page<BookListResponse> readBookList(Pageable pageable) {
+        List<BookListResponse> content = jpaQueryFactory.select(
+                        Projections.constructor(BookListResponse.class,
+                                qBook.id,
+                                qBook.title,
+                                qBook.price,
+                                qBook.sellingPrice,
+                                qBook.author,
+                                qTotalImage.url))
+                .from(qBook)
+                .leftJoin(qBookImage)
+                .on(qBookImage.book.id.eq(qBook.id).and(qBookImage.type.eq(BookImageType.MAIN)))
+                .leftJoin(qTotalImage)
+                .on(qTotalImage.bookImage.id.eq(qBookImage.id))
+                .leftJoin(qBookLike).on(qBookLike.book.id.eq(qBook.id))
+                .groupBy(qBook.id, qBook.title, qBook.price, qBook.sellingPrice, qBook.author, qTotalImage.url)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(getSort(pageable.getSort()))
+                .fetch();
+        long total = Optional.ofNullable(
+                jpaQueryFactory.select(qBook.count())
+                        .from(qBook)
+                        .fetchOne()
+        ).orElse(0L);
+        return new PageImpl<>(content, pageable, total);
+    }
 
-	@Override
-	public ReadBookResponse readDetailBook(Long bookId) {
-		List<ReadBookResponse> content = jpaQueryFactory.select(Projections.constructor(ReadBookResponse.class,
-				qBook.id,
-				qBook.title,
-				qBook.description,
-				qBook.publishedDate,
-				qBook.price,
-				qBook.quantity,
-				qBook.sellingPrice,
-				qBook.viewCount,
-				qBook.packing,
-				qBook.author,
-				qBook.isbn,
-				qBook.publisher,
-				qTotalImage.url))
-			.from(qBook)
-			.leftJoin(qBookImage)
-			.on(qBookImage.book.id.eq(qBook.id).and(qBookImage.type.eq(BookImageType.MAIN)))
-			.leftJoin(qTotalImage)
-			.on(qTotalImage.bookImage.id.eq(qBookImage.id))
-			.where(qBook.id.eq(bookId))
-			.limit(1)
-			.fetch();
 
-		if (content.isEmpty()) {
-			throw new NotExistsBook();
-		}
-		return content.getFirst();
-	}
+    /**
+     * Spring Data JPA Sort 객체를 OrderSpecifier 배열로 변환하는 메서드입니다.
+     * QueryDSL 쿼리에서 정렬을 적용할 수 있습니다.
+     *
+     * @param sort 정렬 기준을 나타내는 Sort 객체
+     * @return 정렬 기준에 따라 정렬된 OrderSpecifier 배열
+     * @throws IllegalArgumentException 정렬 기준이 잘못된 경우
+     * @author 김은비
+     */
+    private OrderSpecifier<?>[] getSort(Sort sort) {
+        return sort.stream()
+                .map(order -> {
+                    String property = order.getProperty();
+                    boolean isAscending = order.isAscending();
+                    log.info("Sorting by property: {}", property);
+                    log.info("Is ascending: {}", isAscending);
 
-	@Override
-	public Page<BookManagementResponse> readAdminBookList(Pageable pageable) {
-		List<BookManagementResponse> content = jpaQueryFactory.select(
-				Projections.constructor(BookManagementResponse.class,
-					qBook.id,
-					qBook.title,
-					qBook.price,
-					qBook.sellingPrice,
-					qBook.author,
-					qBook.quantity,
-					qBook.viewCount))
-			.from(qBook)
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
-			.fetch();
-		long total = Optional.ofNullable(
-			jpaQueryFactory.select(qBook.count())
-				.from(qBook)
-				.fetchOne()
-		).orElse(0L);
-		return new PageImpl<>(content, pageable, total);
-	}
+                    switch (property) {
+                        case "viewCount":
+                            return new OrderSpecifier<>(
+                                    isAscending ? Order.ASC : Order.DESC,
+                                    qBook.viewCount);
+                        case "likes":
+                            return new OrderSpecifier<>(
+                                    isAscending ? Order.ASC : Order.DESC,
+                                    qBookLike.count());
+                        case "publishedDate":
+                            return new OrderSpecifier<>(
+                                    isAscending ? Order.ASC : Order.DESC,
+                                    qBook.publishedDate);
+                        case "price":
+                            return new OrderSpecifier<>(
+                                    isAscending ? Order.ASC : Order.DESC,
+                                    qBook.price);
+                        default:
+                            throw new IllegalArgumentException("정렬 기준이 잘못되었습니다!!: " + property);
+                    }
+                })
+                .toArray(OrderSpecifier[]::new);
+    }
+
+
+    /**
+     * 도서 상세 보기 쿼리입니다.
+     *
+     * @param bookId 북 아이디
+     * @return 도서 상세 정보
+     * @author 한민기
+     */
+    @Override
+    public ReadBookResponse readDetailBook(Long bookId) {
+        List<ReadBookResponse> content = jpaQueryFactory.select(Projections.constructor(ReadBookResponse.class,
+                        qBook.id,
+                        qBook.title,
+                        qBook.description,
+                        qBook.publishedDate,
+                        qBook.price,
+                        qBook.quantity,
+                        qBook.sellingPrice,
+                        qBook.viewCount,
+                        qBook.packing,
+                        qBook.author,
+                        qBook.isbn,
+                        qBook.publisher,
+                        qTotalImage.url))
+                .from(qBook)
+                .leftJoin(qBookImage)
+                .on(qBookImage.book.id.eq(qBook.id).and(qBookImage.type.eq(BookImageType.MAIN)))
+                .leftJoin(qTotalImage)
+                .on(qTotalImage.bookImage.id.eq(qBookImage.id))
+                .where(qBook.id.eq(bookId))
+                .limit(1)
+                .fetch();
+
+        if (content.isEmpty()) {
+            throw new NotExistsBook();
+        }
+        return content.getFirst();
+    }
+
+    /**
+     * 관리자 페이지에서 도서 정보를 불러오는 쿼리입니다.
+     *
+     * @param pageable 페이지 객체
+     * @return 도서 리스트
+     * @author 한민기
+     */
+    @Override
+    public Page<BookManagementResponse> readAdminBookList(Pageable pageable) {
+        List<BookManagementResponse> content = jpaQueryFactory.select(
+                        Projections.constructor(BookManagementResponse.class,
+                                qBook.id,
+                                qBook.title,
+                                qBook.price,
+                                qBook.sellingPrice,
+                                qBook.author,
+                                qBook.quantity,
+                                qBook.viewCount))
+                .from(qBook)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        long total = Optional.ofNullable(
+                jpaQueryFactory.select(qBook.count())
+                        .from(qBook)
+                        .fetchOne()
+        ).orElse(0L);
+        return new PageImpl<>(content, pageable, total);
+    }
 
 }
 
