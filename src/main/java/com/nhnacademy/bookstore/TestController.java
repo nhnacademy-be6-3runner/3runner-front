af5fbde5b27435229501fe408104336a1eeab1f6
한민기 <dkssudrhd@naver.com>
2024-07-08 09:19:02 +0900

    feature : log back

--- src/main/java/com/nhnacademy/bookstore/TestController.java
+++ src/main/java/com/nhnacademy/bookstore/TestController.java
@@ -1,7 +1,8 @@
 package com.nhnacademy.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +10,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class TestController {
-	@GetMapping("/bookstore/test")
-	public String test(@RequestHeader(name = "Member-Id") String memberId) {
-		log.warn("Member-Id Header: {}", memberId);
+
+	@GetMapping("/test")
+	public String test() {
+		Logger logger = LoggerFactory.getLogger("racingcar");
+		logger.debug("Hello world. debug");
+		logger.warn("Hello world. warn");
+		logger.error("Hello world. error");
+		logger.info("Hello world. info");
+
+		log.info("This is an info message");
+		log.error("This is an error message");
+		log.debug("This is a debug message");
 		return "test!";
 	}
+
+	@GetMapping("/exception")
+	public String exception() {
+		throw new RuntimeException("Test exception");
+	}
+
 }
