7ee709b5e36942f790037d565b0cb12ec22a9a79
administrator <administrator@administrators-Laptop.local>
2024-07-09 16:59:25 +0900

    hotfix:cors

--- src/main/java/com/nhnacademy/front/config/WebConfig.java
+++ src/main/java/com/nhnacademy/front/config/WebConfig.java
@@ -37,8 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
 				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
+						.allowedHeaders("*");
 			}
 		};
 	}
