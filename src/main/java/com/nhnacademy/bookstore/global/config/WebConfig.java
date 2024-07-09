c659acdc258a73d3361c3906905b79ddba3b7ced
administrator <administrator@administrators-Laptop.local>
2024-07-09 18:17:58 +0900

    hotfix:cors

---
+++ src/main/java/com/nhnacademy/bookstore/global/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.bookstore.global.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebConfig
+ * interceptor 설정
+ *
+ * @author 오연수
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://3runner.shop")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+}
