ec7768c22cf331c2680044fea4341c20457fb4e9
administrator <administrator@administrators-Laptop.local>
2024-07-09 16:59:06 +0900

    hotfix:cors

--- src/main/java/com/nhnacademy/bookstore/global/config/WebConfig.java
+++ src/main/java/com/nhnacademy/bookstore/global/config/WebConfig.java
@@ -31,10 +31,9 @@ public class WebConfig implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://3runner.shop")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
+						.allowedHeaders("*");
 			}
 		};
 	}
