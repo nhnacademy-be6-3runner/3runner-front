20b638cf14f5f51ca2424afcbed34b3d1ab3de1f
administrator <administrator@administrators-Laptop.local>
2024-07-09 15:01:02 +0900

    hotfix:cors

--- src/main/java/com/nhnacademy/bookstore/global/config/WebConfig.java
+++ src/main/java/com/nhnacademy/bookstore/global/config/WebConfig.java
@@ -16,15 +16,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("*")
+						.allowedOrigins("http://133.186.143.40")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
