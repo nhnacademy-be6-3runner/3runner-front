39ddecb5ad43bcee73846f77217a4572f693d57a
한민기 <dkssudrhd@naver.com>
2024-07-10 11:13:50 +0900

    feature : keyManager

--- src/main/java/com/nhnacademy/bookstore/TestController.java
+++ src/main/java/com/nhnacademy/bookstore/TestController.java
@@ -1,21 +1,27 @@
 package com.nhnacademy.bookstore;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nhnacademy.bookstore.global.keyManager.manager.KeyManager;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+	private final KeyManager keyManager;
 
 	@GetMapping("/test")
 	public String test() {
-		Logger logger = LoggerFactory.getLogger("errorLogger");
-		logger.error("3Runner 화이팅!");
-		return "test!";
+		// Logger logger = LoggerFactory.getLogger("errorLogger");
+		// logger.error("3Runner 화이팅!");
+		// KeyManagerResponse key = keyManager.getKey("b6b9d1cb45ab4fb1b46637352b6a053c");
+
+		// return key.getBody().getSecret();
+		return "test";
 	}
 
 	@GetMapping("/exception")
